bdd4d55d100337a20738ef425136f283da35afc9
Iain Rosen <iain.rosen@gmail.com>
2020-01-14 10:47:11 -0800

    Rewrote setup script

--- setup.py
+++ setup.py
@@ -2,18 +2,24 @@ import os
 import sys
 import time
 import sqlite3
-if os.path.exists("/usr/bin/downtime/db/botconfig.db") == True and sys.argv[1] == "check":
-    return 0
-else:
-    return 1
-if sys.argv[1] == "init":
+def checkconf():
+    if os.path.exists("/usr/bin/downtime/db/botconfig.db") == True:
+        return 0
+    else:
+        return 1
+def init(key):
     os.system("rm -rf /usr/bin/downtime/db/*")
     conn = sqlite3.connect("/usr/bin/downtime/db/botconfig.db")
     c = conn.cursor()
     c.execute('''CREATE TABLE api
     (apikey text)''')
-    key = sys.argv[2]
     insertCommand = "INSERT INTO api VALUES ('" + key + "')"
     c.execute(insertCommand)
     conn.commit()
     return 0
+if sys.argv[1] == "init":
+    init(sys.agrv[2])
+elif sys.argv[1] == "check":
+    check()
+else:
+    print("Argument missing or not recognized.")
