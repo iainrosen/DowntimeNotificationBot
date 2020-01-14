5cd996afbe58dedf4f757cf1a031468d1e9e6f70
Iain Rosen <iain.rosen@gmail.com>
2020-01-14 13:32:25 -0800

    Added central database abstraction script

---
+++ install/dbget.py
@@ -0,0 +1,19 @@
+import telepot
+import sqlite3
+def readval(value, table):
+    conn = sqlite3.connect("/usr/bin/downtime/db/botconfig.db")
+    c = conn.cursor()
+    command = "SELECT " + value + " FROM " + table
+    c.execute(command)
+    configFetch = c.fetchone()
+    return configFetch[0]
+def writeval(value, table):
+    try:
+        conn = sqlite3.connect("/usr/bin/downtime/db/botconfig.db")
+        c = conn.cursor()
+        command = "INSERT INTO " + table + " VALUES (" + value + ")"
+        c.execute(command)
+        conn.commit()
+        return 0
+    except:
+        return 1
