7aeb5a0da6333f8607efa34665a360a7dfca7659
Iain Rosen <iain.rosen@gmail.com>
2020-01-15 09:38:47 -0800

    Update watchbot.py

--- install/watchbot.py
+++ install/watchbot.py
@@ -8,11 +8,15 @@ hname = socket.gethostname()
 netfail = 0
 servicefail = 0
 servicewatch = ["downtime", "apache2"] #add the services you want to watch here
-
+failed = []
 while True:
     #check for server failures
     for i in servicewatch:
         checksvc = "systemctl is-active " + i
-        if (subprocess.getoutput(checksvc)) == "inactive":
-            print("Service Failed!")
+        if (subprocess.getoutput(checksvc)) == "inactive" and i not in failed:
+            failed.append(i)
+
+        else:
+            failed.remove(i)
+
     time.sleep(2)
