75e8a7219891092bda3813b95cef6100870944a1
Iain Rosen <iain.rosen@gmail.com>
2020-01-15 18:44:57 -0800

    Update outgoing.py

--- install/outgoing.py
+++ install/outgoing.py
@@ -4,6 +4,10 @@ import telepot
 import sqlite3
 import dbget
 def sendmsg(userid, message):
-    token = dbget.readval("*", "api")
-    bot = telepot.Bot(token)
-    bot.sendMessage(userid, message)
+    try:
+        token = dbget.readval("*", "api")
+        bot = telepot.Bot(token)
+        bot.sendMessage(userid, message)
+        return 0
+    except:
+        return 1
