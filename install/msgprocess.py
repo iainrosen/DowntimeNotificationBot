700f5516534c5d96d5009a035a243b85a80a87a3
Iain Rosen <iain.rosen@gmail.com>
2020-01-18 22:01:01 -0800

    Update msgprocess.py

--- install/msgprocess.py
+++ install/msgprocess.py
@@ -8,6 +8,13 @@ import subprocess
 joinRunning = []
 starttext = '''Welcome to Downtime!
 Before you start, put your server in registration mode by typing "downtime-cli register". Then, tap here -> /register'''
+helptext = '''Available Commands:
+/register           Register with Downtime Server
+/help               View this helptext
+/status             View the Downtime Server status
+/restart [service]  Restart a specified service
+/whoami             View your userid
+'''
 def sendmsg(userid, message):
     token = dbget.readval("*","api")
     bot = telepot.Bot(token)
@@ -33,13 +40,12 @@ def process(usrid, text):
         else:
             sendmsg(usrid, "Registration Unavailable.")
     elif text == "/help":
-        sendmsg(usrid, "Good Luck!")
+        sendmsg(usrid, helptext)
     elif text == "/whoami":
         msgSend = "Your User ID is: " + str(usrid)
         sendmsg(usrid, msgSend)
     #priv commands
     elif text == "/status" and priv == True:
-        sendmsg(usrid, "Checking status of Downtime...")
         stats = subprocess.getoutput("systemctl status downtime")
         sendmsg(usrid, stats)
     elif "/restart" in text and priv == True:
