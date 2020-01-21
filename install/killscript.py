f572274b273195bc3e1b5da78a5ff6f4fc3dc2e2
Iain Rosen <iain.rosen@gmail.com>
2020-01-20 23:52:05 -0800

    Update killscript.py

--- install/killscript.py
+++ install/killscript.py
@@ -4,4 +4,15 @@ import socket
 hname = socket.gethostname()
 userid = dbget.readval("*", "authusers")
 msg = "Downtime on " + hname + " is stopping by systemd."
-outgoing.sendmsg(userid, msg)
+def sendmsg(userid, message):
+    try:
+        token = dbget.readval("*", "api")
+        bot = telepot.Bot(token)
+        message = hname + ": " + message
+        bot.sendMessage(userid, message)
+        dlog.info("Sent message: " + message + "to userid: " + str(userid))
+        return 0
+    except:
+        dlog.critical("Failed to send message to: " + str(userid) + ". Message was: " + message)
+        return 1
+sendmsg(userid, msg)
