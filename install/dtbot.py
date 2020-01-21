d63e3a97546ae16b73a3c00a64305d4599848406
Iain Rosen <iain.rosen@gmail.com>
2020-01-20 23:34:42 -0800

    Implemented logging in dtbot.py

--- install/dtbot.py
+++ install/dtbot.py
@@ -5,6 +5,8 @@ import sqlite3
 import dbget
 import socket
 import subprocess
+import dlog
+dlog.info("Starting DowntimeBot...")
 token = dbget.readval("*", "api")
 bot = telepot.Bot(token)
 lastMsg = 1
@@ -14,10 +16,11 @@ Before you start, put your server in registration mode by typing "downtime-cli r
 helptext = '''Available Commands:
 /register           Register with Downtime Server
 /getupdates         Get updates on the server with aptitude
+/doupdates          Update the server's packages with aptitude
 /help               View this helptext
 /status             View the Downtime Server status
 /restart [x]        Restart a specified service
-/whoami             View your userid
+/whoami             View your userid (DEPRICATED)
 '''
 def sendmsg(userid, message):
     try:
@@ -25,34 +28,39 @@ def sendmsg(userid, message):
         bot = telepot.Bot(token)
         message = hname + ": " + message
         bot.sendMessage(userid, message)
+        dlog.info("Sent message: " + message + "to userid: " + userid)
         return 0
     except:
+        dlog.critical("Failed to send message to: " + userid + ". Message was: " + message)
         return 1
 def process(usrid, text):
     if str(usrid) == (dbget.readval("*", "authusers")):
         priv = True
+        dlog.info("Recieved message: " + text + " from privileged user: " + usrid)
     else:
         priv = False
+        dlog.info("Recieved message: " + text + " from unprivileged user: " + usrid)
     #nonpriv commands
     if text == "/start":
         sendmsg(usrid, starttext)
     elif text == ("/register " + hname):
         if (dbget.readval("*", "authusers") != 1):
+            dlog.warning("User initiated registration, but was already registered. Userid: " + usrid)
             sendmsg(usrid, "User already registered!")
             os.system("rm -rf /tmp/registration.downtime.lock")
             return 0
         if (os.path.exists("/tmp/registration.downtime.lock")) == True:
+            dlog.info("Registering new user: " + usrid + "...")
             cmd = "python3 /usr/bin/downtime/setup.py newuser " + str(usrid)
             os.system(cmd)
             os.system("rm -rf /tmp/registration.downtime.lock")
             sendmsg(usrid, "Registration Complete!")
+            dlog.info("Registered " + usrid)
         else:
+            dlog.warning("User attempted to register but registration was unavailable. Userid: "+usrid)
             sendmsg(usrid, "Registration Unavailable.")
-    elif text == ("/register " + hname):
+    elif text == ("/help " + hname):
         sendmsg(usrid, helptext)
-    elif text == "/whoami":
-        msgSend = "Your User ID is: " + str(usrid)
-        sendmsg(usrid, msgSend)
     #priv commands
     elif text == "/status" and priv == True:
         stats = subprocess.getoutput("systemctl status downtime")
@@ -62,20 +70,25 @@ def process(usrid, text):
         os.system("python3 /usr/bin/downtime/update-notf.py force &")
     elif text == ("/doupdates " + hname) and priv == True:
         sendmsg(usrid, "Executing updates on " + hname)
+        dlog.info("Executing updates as per command of user: ", usrid)
         os.system("apt upgrade -y &")
     elif "/restart" in text and priv == True:
         svstart = text.rsplit(' ')
         if svstart[1] == hname and svstart[2]:
+            dlog.info(usrid + " initiated restart of " + svstart[2] + ".")
             sendmsg(usrid, "Attempting to start " + svstart[1])
             cmd = "systemctl restart " + svstart[1]
             os.system(cmd)
             stats = subprocess.getoutput("systemctl is-active " + svstart[1])
             if stats == "active":
+                dlog.info("Service restarted successfully")
                 sendmsg(usrid, svstart[1] + " start complete.")
             else:
+                dlog.warning("Service restart failed.")
                 sendmsg(usrid, svstart[1] + " start failed.")
     else:
         if hname in text:
+            dlog.warning("User, " + usrid + " attempted to access an unauthorized or unknown command.")
             sendmsg(usrid, "You might not be allowed to access that command yet.")
 def parseMsg(msg):
     parse = message[0]
@@ -88,6 +101,8 @@ def parseMsg(msg):
         usrid = fromDetails.get("id")
         text = messageDetails.get("text")
         return updateid, usrid, text
+    if messageDetails == False:
+        editedMsg = parse.get("edited")
     return updateid, 0, 0
 userid = dbget.readval("*", "authusers")
 msg = "Downtime on " + hname + " is starting by systemd."
@@ -95,6 +110,7 @@ sendmsg(userid, msg)
 while True:
         message = bot.getUpdates(offset=lastMsg)
         if message != []:
+            dlog.info("RAW MESSAGE DUMP: " + message)
             updateid, usrid, text = parseMsg(message)
             lastMsg = updateid + 1
             if usrid != 0:
